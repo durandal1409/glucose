0e407b0b88f29862cab500824a59f360c502a740
kladovamaria <mackladova@ya.ru>
2021-06-24 22:26:04 +0300

    prepared for heroku

--- app.js
+++ app.js
@@ -59,7 +59,11 @@ app.post("/", function(req, res) {
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
-    console.log("server has started on port 3000")
+app.listen(port, function() {
+    console.log("Server has started successfully.")
 });
